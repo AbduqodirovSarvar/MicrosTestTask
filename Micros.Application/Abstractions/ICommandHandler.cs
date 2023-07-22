27b261a036305872e7c4b47bc34f176fa4b41a7e
Sarvar Abduqodirov <abduqodirovsarvar.2002@gmail.com>
2023-07-22 22:27:56 +0500

    Added iterfaces for mediatr

---
+++ Micros.Application/Abstractions/ICommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Micros.Application.Abstractions
+{
+    public interface ICommandHandler<in TRequest, TResponse> : IRequestHandler<TRequest, TResponse> where TRequest : ICommand<TResponse>
+    {
+    }
+}
