713b71d4b91966e2d9041f05b8ee95146aa7298d
Sarvar Abduqodirov <abduqodirovsarvar.2002@gmail.com>
2023-07-22 21:34:24 +0500

    added dbcontext

---
+++ Micros.Infrastucture/DbContexts/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Micros.Application.Abstractions;
+using Micros.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Micros.Infrastucture.DbContexts
+{
+    public class AppDbContext : DbContext, IAppDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) 
+            : base(options) { }
+        public DbSet<User> Users { get; set; }
+        public DbSet<InCome> InComes { get; set; }
+        public DbSet<OutCome> OutComes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+    }
+}
---
+++ Micros.Infrastucture/DepencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Micros.Application.Abstractions;
+using Micros.Infrastucture.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Micros.Infrastucture
+{
+    public static class DepencyInjection
+    {
+        public static IServiceCollection InfrastructureServices(this IServiceCollection _services, IConfiguration _configuration)
+        {
+            _services.AddDbContext<AppDbContext>(options
+                => options.UseNpgsql(_configuration.GetConnectionString("DefaultConnection")));
+
+            _services.AddScoped<IAppDbContext, AppDbContext>();
+            return _services;
+        }
+    }
+}
