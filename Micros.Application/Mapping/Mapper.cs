62a9c081a0200f5053de675dde31e95e6ea32029
Sarvar Abduqodirov <abduqodirovsarvar.2002@gmail.com>
2023-07-22 22:21:00 +0500

    added models

---
+++ Micros.Application/Mapping/Mapper.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Micros.Application.Models.ViewModels;
+using Micros.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Micros.Application.Mapping
+{
+    public class Mapper : Profile
+    {
+        public Mapper() 
+        {
+            CreateMap<User, UserViewModel>();
+            CreateMap<InCome, InComeViewModel>();
+            CreateMap<OutCome, OutComeViewModel>();
+        }
+    }
+}
