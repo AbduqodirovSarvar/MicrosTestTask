df03776674f13c0a3e8cb7b6244b275a8c4d99e3
Sarvar Abduqodirov <abduqodirovsarvar.2002@gmail.com>
2023-07-23 15:13:08 +0500

    created controllers

---
+++ Micros.Api/Controllers/inComeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Micros.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class inComeController : ControllerBase
+    {
+    }
+}
